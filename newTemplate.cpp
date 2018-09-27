0463c0748588eff988fc7f7c85e7c9dd0585e97a
Abdelrhman98 <abdelrhman.soleman98@gmail.com>
2018-09-27 12:49:56 +0200

    *_*

---
+++ newTemplate.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vll vector<ll>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define vs vector<string>
+#define vb vector<bool>
+#define vi vector<int>
+#define vd vector<double>
+#define vvi vector< vector<int> >
+#define vvii vector< vector< pii > >
+#define ld long double
+#define mapii map<int, int>
+#define mapsi map<string, int>
+#define erep(i, x, n) for (auto i = x; i<=(ll)(n); i++)
+#define rep(i, x, n) for(auto i = x; i<(ll)(n); i++)
+#define INF  LLONG_MAX
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define pie  acos(-1)
+#define mod(n,m) ((n % m + m) % m)
+#define eps (1e-6)
+#define reset(n, m) memset(n, m, sizeof n)
+#define mp(x, y, z) {x, {y, z}}
+#define INF_MAX 2147483647
+#define INF_MIN -2147483647
+
+#define srtva(v) sort(all(v))
+#define srtvd(v) sort(v.rbegin(),v.rend())
+#define srtaa(a,n) sort(a,a+n)
+#define srtad(a,n) sort(a,a+n),reverse(a,a+n)
+
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfi4(v1,v2,v3,v4) scanf("%d %d %d %d",&v1,&v2,&v3,&v4)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfll4(v1,v2,v3,v4) scanf("%I64d %I64d %I64d %I64d",&v1,&v2,&v3,&v4)
+#define sfstr(v) scanf("%s", v)
+#define sfch1(ch) scanf("%c",&ch)
+#define chr getchar()
+#define pfi1(x) printf("%d",x)
+#define pfi2(x,y) printf("%d %d",x,y)
+#define pfl1(x) printf("%lld",x)
+#define pfl2(x,y) printf("%lld %lld",x,y)
+#define pfstr(ch) printf("%s",ch)
+#define pfch(ch) printf("%c",ch)
+#define sf scanf
+#define pf printf
+
+void PLAY() {
+	cout << fixed << setprecision(4);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+#define FI freopen("input.txt","r",stdin)
+#define FO freopen("out.txt","w",stdout)
+#define nl printf("\n")
+#define SS stringstream
+
+#define pb push_back
+#define mp make_pair
+
+#define debug(x) cout<<#x<<" = { "<<x<<" }\n"
+
+#define FOR(i,s,e) for(int i = s; i < e; i++)
+#define FORD(i,e,s) for(int i=e-1,i>=s;i--)
+#define lp(i,n) FOR(i,0,n)
+#define lpd(i,n) FORD(i,n-1,0)
+#define rev(i,s,e) FORD(i,s,e)
+#define rep(i,v) FOR(i,0,sz(v))
+
+LL gcd(LL a, LL b) { return !b ? a : gcd(b, a % b); }
+LL lcm(LL a, LL b) { return (a / gcd(a, b)) * b; }
+ 
+#define ssz(a) strlen(a)
+int  main(){
+	
+	return 0;
+}
